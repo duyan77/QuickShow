03c428b607b5ba040066d57e51887ea4f416941b
duyan77 <anbui5948@gmail.com>
2025-08-16 07:15:57 +0700

    thêm tính năng gửi mail

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
@@ -9,14 +9,14 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail = async ({ to, subject, body })=>{
-    const response = await transporter.sendMail({
-        from: process.env.SENDER_EMAIL,
-        to,
-        subject,
-        html: body,
-    })
-    return response
-}
+const sendEmail = async ({ to, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
+  return response;
+};
 
 export default sendEmail;
